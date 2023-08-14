089fb2f330b88f2097c2ee079d2d992a48342ee0
jorgedeltoro111 <ingjorgedeltoro@gmail.com>
2023-08-13 23:50:07 -0600

    Creación de nuestra funcion que retorna nuestro componente conforme a la ruta que se establecio y mandandolo a llamar en la funcion principal App con BrowserRouter para poder recibir lo que retorno nuestra funcion.

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -1,21 +1,35 @@
-import './App.css';
+import { useRoutes, BrowserRouter } from 'react-router-dom';
 import Home from '../Home';
 import MyOrder from '../MyOrder';
 import MyOrders from '../MyOrders';
 import NotFound from '../NotFound';
 import SignIn from '../SignIn';
 import Account from '../Account';
+import './App.css';
+
+/**Con esta funcion vamos a crear un array de rutas que van a devolver a nuestro componente
+ * lo que deseamos es querer darle una ruta a cada componente y retornalo para mostrarlo.
+ * y en la funcion principal "App" lo vamos a recibir.
+ */
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: '/', element: <Home/> },
+    { path: '/my-order', element: <MyOrder/> },
+    { path: '/my-orders', element: <MyOrders/> },
+    { path: '/*', element: <NotFound/> },
+    { path: '/sign-in', element: <SignIn/> },
+    { path: '/my-account', element: <Account/> },
+  ])
+
+  return routes;
+}
 
 function App() {
+  
   return (
-    <div className='bg-red-100'>
-      <Home/>
-      <Account/>
-      <MyOrder/>
-      <MyOrders/>
-      <SignIn/>
-      <NotFound/>
-    </div>
+    <BrowserRouter>
+      <AppRoutes/>
+    </BrowserRouter>
   )
 }
 
