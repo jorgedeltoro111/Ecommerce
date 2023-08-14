c4d765e641512f305d7d8a1d42dd4860b3c058db
jorgedeltoro111 <ingjorgedeltoro@gmail.com>
2023-08-13 22:56:28 -0600

    Initial commit

---
+++ src/pages/App/index.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='bg-red-100'>
+      Bienvenido al curso de tailwind con vite, tailwind y react.
+    </div>
+  )
+}
+
+export default App
