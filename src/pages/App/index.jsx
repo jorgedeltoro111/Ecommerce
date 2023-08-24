e190656c8e03e3c5c032a408ccdbe19d59f6f97e
jorgedeltoro111 <ingjorgedeltoro@gmail.com>
2023-08-24 13:26:35 -0600

    Agregue el componente de navBar

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -5,6 +5,7 @@ import MyOrders from '../MyOrders';
 import NotFound from '../NotFound';
 import SignIn from '../SignIn';
 import Account from '../Account';
+import NavBar from '../../Components/NavBar/index';
 import './App.css';
 
 /**Con esta funcion vamos a crear un array de rutas que van a devolver a nuestro componente
@@ -29,6 +30,7 @@ function App() {
   return (
     <BrowserRouter>
       <AppRoutes/>
+      <NavBar/>
     </BrowserRouter>
   )
 }
