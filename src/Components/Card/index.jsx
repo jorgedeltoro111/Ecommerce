c1d1f44770d5cee74afd7b8ccab3f1d60045b58e
jorgedeltoro111 <ingjorgedeltoro@gmail.com>
2023-08-29 20:21:19 -0600

    Consumo de APIFake y algunas modificaciones de estiles CSS

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 
-function Card() {
+function Card({product}) {
   return (
-    <div className='bg-white cursor-pointer w-56 h-60 rounded-lg border-2'>
+    <div className='p-2 bg-white cursor-pointer w-56 h-60 rounded-lg border-2 mt-3'>
         <figure className='relative mb-2 w-full h-4/5'>
-            <span className='absolute bottom-0 mb-2 ml-2 p-1 left-0 bg-white/60 rounded-lg text-black text-sm'>Electronics</span>
-            <img className='w-full h-full object-cover' src='https://images.pexels.com/photos/5726030/pexels-photo-5726030.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt='HeadPhones'/>
-            <div className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 font-black'>
+            <span className='absolute bottom-0 mb-2 ml-2 p-1 left-0 bg-white/60 rounded-lg text-black text-sm'>{product.category}</span>
+            <img className='h-full' src={product.image} alt={product.title} />
+            <div className='border border-black  absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 font-black'>
                 +
             </div>
         </figure>
         <p className='flex justify-between'>
-            <span className='text-sm font-light m-2'>HeadPhones</span>
-            <span className='text-md font-black m-2'>$3000</span>
+            <span className='text-sm truncate font-light m-2'>{product.title}</span>
+            <span className='text-md font-black m-2'>${product.price}</span>
         </p>
     </div>
   )
