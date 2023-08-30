97768d7e9524f0e2f5f50d59e5426c17244712b7
jorgedeltoro111 <ingjorgedeltoro@gmail.com>
2023-08-29 20:21:51 -0600

    Creacion de un container para poder mostrarlos en columnas con estilos CSS

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Layout from '../../Components/Layout/index';
 import Card from '../../Components/Card/index';
 function Home() {
+  const [products, setProducts] = useState([]);
+  
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(response => {
+        if(!response.ok){
+          throw new Error('Network response was not ok');
+        }else{
+          return response.json();
+        } 
+      })
+      .then(data => setProducts(data))
+      .catch(error => {
+        console.log(error);
+      })
+  }, [])
+  
   return (
     <Layout>
-      <div>home</div>
-      <Card/>
+      Home
+      <div className='grid grid-cols-4 gap-4'>
+        {
+          products?.map((product) => {
+            return <Card key={product.id} product={product}/>
+          })
+        }
+      </div>
+      
     </Layout>
   )
 }
