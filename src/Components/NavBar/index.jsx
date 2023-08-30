a759fda7308e52a031072e835ebfc521944b3926
jorgedeltoro111 <ingjorgedeltoro@gmail.com>
2023-08-29 20:20:28 -0600

    Coloque el fondo en blanco para cuando deslice se mantenga visible la navbar

--- src/Components/NavBar/index.jsx
+++ src/Components/NavBar/index.jsx
@@ -3,7 +3,7 @@ import {NavLink} from 'react-router-dom';
 const NavBar = () => {
     const activeStyle = 'underline underline-offset-4';
     return(//w-full significa que tome el width en todo lo ancho
-        <nav className='flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-light top-0'>{/*Aqui va la parte de la izquierda del navbar */}
+        <nav className='flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-light top-0 bg-white'>{/*Aqui va la parte de la izquierda del navbar */}
             <ul className='flex items-center gap-3'>
                 <li className='font-semibold text-lg'>
                     <NavLink to='/'>
